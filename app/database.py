de674b7c28178578c90b01216ff869fb090d43fd
Vasilije Tomovic <vasilije.tomovic@symphony.is>
2026-04-07 16:26:09 +0200

    database session DRY approach

--- app/database.py
+++ app/database.py
@@ -6,12 +6,12 @@ engine = create_async_engine(
     echo=True,
 )
 
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine,
-    expire_on_commit=False,
-    class_=AsyncSession
-)
-
 async def get_session():
-    async with AsyncSessionLocal() as session:
+    async_session = async_sessionmaker(
+        bind=engine, 
+        expire_on_commit=False, 
+        class_=AsyncSession
+    )
+
+    async with async_session() as session:
         yield session
