e78077a2e94237c5578e096d6bd49f28c7883e8b
Vasilije Tomovic <vasilije.tomovic@symphony.is>
2026-04-07 16:28:29 +0200

    ipak je bolje ovako...

--- app/database.py
+++ app/database.py
@@ -6,12 +6,12 @@ engine = create_async_engine(
     echo=True,
 )
 
-async def get_session():
-    async_session = async_sessionmaker(
-        bind=engine, 
-        expire_on_commit=False, 
-        class_=AsyncSession
-    )
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
 
-    async with async_session() as session:
+async def get_session():
+    async with AsyncSessionLocal() as session:
         yield session
